1b467bb5fc1c42102b718125c20a2758558fb75c
Will Davies <wdavies973@gmail.com>
2024-03-28 00:14:51 -0500

    Add game loop + clean up rendering

--- app/src/main/java/com/fouram/snake/Renderer.java
+++ app/src/main/java/com/fouram/snake/Renderer.java
@@ -4,33 +4,37 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class Renderer extends View {
-    private Path path = new Path();
-    private Paint paint = new Paint();
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class Renderer extends View implements Runnable {
 
-    //Constructor
-    public Renderer(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        setFocusable(true);
-        setFocusableInTouchMode(true);
-        PaintSettings();
+    // This is your update function, it's called rapidly
+    public void update() {
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawPath(path, paint);
-    }
 
+        /*
+         * Clear
+         */
+
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+
+        canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
+
+
+    }
 
     // Get x and y and follow user motion events
     @Override
@@ -41,11 +45,9 @@ public class Renderer extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 // Starts a new line in the path
-                path.moveTo(pointX, pointY);
                 break;
             case MotionEvent.ACTION_MOVE:
                 // Draws line between last point and this point
-                path.lineTo(pointX, pointY);
                 break;
             default:
                 return false;
@@ -55,14 +57,28 @@ public class Renderer extends View {
         return true;
     }
 
+    /*
+     *
+     */
 
-    private void PaintSettings() {
-
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(10);
-
-
+    private Paint paint = new Paint();
+    public Renderer(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
     }
 
+    @Override
+    public void run() {
+        int FPS = 500;
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                update();
+                postInvalidate();
+            }
+        }, 0, (int)(1000.0 / (double)FPS));
+    }
 }
