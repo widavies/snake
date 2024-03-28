2e3851cfb99ee7ee6778c218843212e4a3669f1c
Will Davies <wdavies973@gmail.com>
2024-03-27 23:50:14 -0500

    Initial commit

---
+++ app/src/main/java/com/fouram/snake/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.fouram.snake
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fouram.snake.ui.theme.SnakeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContent {
+            Renderer(baseContext, null);
+        }
+    }
+}
---
+++ app/src/main/java/com/fouram/snake/Renderer.java
@@ -0,0 +1,68 @@
+package com.fouram.snake;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class Renderer extends View {
+    private Path path = new Path();
+    private Paint paint = new Paint();
+
+
+    //Constructor
+    public Renderer(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        PaintSettings();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawPath(path, paint);
+    }
+
+
+    // Get x and y and follow user motion events
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX = event.getX();
+        float pointY = event.getY();
+        // Checks for the event that occurs
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                // Starts a new line in the path
+                path.moveTo(pointX, pointY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // Draws line between last point and this point
+                path.lineTo(pointX, pointY);
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
+    }
+
+
+    private void PaintSettings() {
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(10);
+
+
+    }
+
+}
